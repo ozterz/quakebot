b316ff832c0d33b1739eb25ddb8c72eb7da4ac7c
Özge Terzioğlu <117301682+ozterz@users.noreply.github.com>
2023-02-09 20:36:33 -0800

    Update quakebot.py
    slayed my demons, solved the challenge

--- scripts/quakebot.py
+++ scripts/quakebot.py
@@ -6,28 +6,17 @@ url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojso
 response = requests.get(url)
 eq = response.json() #can also use json.loads()
 
-eq_list = []
+count = len(eq["features"])
 
-print("There were 9 earthquakes in the last hour:\n")
+print("There were " + str(count) + " earthquakes in the last hour:\n")
 
 for key in range(len(eq["features"])):
   magnitude = eq["features"][key]["properties"]["mag"]
-  eq_list.append(magnitude)
-
   place = eq["features"][key]["properties"]["place"]
-  eq_list.append(place)
 
   for time in range(len(eq["features"])):
     time = eq["features"][key]["properties"]["time"]/1000
     hour = datetime.fromtimestamp(time).strftime("%H:%M:%S")
-    eq_list.append(hour)
     date = datetime.fromtimestamp(time).strftime("%Y-%m-%d")
-    eq_list.append(date)
   
-  "-Magnitude {} {} at {} {} UTC.".format(
-    magnitude,
-    place,
-    date,
-    hour
-  )
   print(f"A magnitude {magnitude} earthquake occured {place} on {date} at {hour}.\n")
